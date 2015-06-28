2dc5ee0a6e851ea341365fbfc0e3d4e947beb30d
Pablo Valor <pablovalor89@gmail.com>
2015-06-28 20:57:05 -0300

    Terminada priemra implementacion de Panel Ofertas

--- js/main.js
+++ js/main.js
@@ -2,9 +2,28 @@ $(document).on('ready', function(){
   "use strict";
 
   //cacheando objetos DOM
-  var $cabecera = $('.cabecera');
-  var $covervid = $('.covervid-video');
+  var $cabecera   = $('.cabecera');
+  var $covervid   = $('.covervid-video');
   var altoVentana = $(window).height();
+  var $tarjetas    = $('.tarjeta');
+
+  $tarjetas.hover(function(){
+     $(this).find('.tarjeta-imagen-contenedor').css({
+        "transform": "scale(2) rotate(20deg)"
+     });
+     $(this).find('.tarjeta-detalle-producto-contenedor').css({
+        "left": "0"
+     });
+  },
+  function(){
+     $(this).find('.tarjeta-imagen-contenedor').css({
+       "transform": "scale(1) rotate(0)"
+     });
+     $(this).find('.tarjeta-detalle-producto-contenedor').css({
+       "left": "-100%"
+     });
+  }
+ );
 
   $covervid.height(altoVentana);
 
