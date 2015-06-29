f75569869fdb427dd2b9048e9625a275c3566b2a
Pablo Valor <pablovalor89@gmail.com>
2015-06-29 00:34:26 -0300

    Agregado cambios en cabecera y Quienes Somos

--- js/main.js
+++ js/main.js
@@ -1,35 +1,36 @@
 $(document).on('ready', function(){
-  "use strict";
+    "use strict";
 
-  //cacheando objetos DOM
-  var $cabecera   = $('.cabecera');
-  var $covervid   = $('.covervid-video');
-  var altoVentana = $(window).height();
-  var $tarjetas    = $('.tarjeta');
+    //cacheando objetos DOM
+    var $cabecera   = $('.cabecera');
+    var $covervid   = $('.covervid-video');
+    var altoVentana = $(window).height();
+    var $tarjetas    = $('.tarjeta');
 
-  $tarjetas.hover(function(){
-     $(this).find('.tarjeta-imagen-contenedor').css({
-        "transform": "scale(2) rotate(20deg)"
-     });
-     $(this).find('.tarjeta-detalle-producto-contenedor').css({
-        "left": "0"
-     });
-  },
-  function(){
-     $(this).find('.tarjeta-imagen-contenedor').css({
-       "transform": "scale(1) rotate(0)"
-     });
-     $(this).find('.tarjeta-detalle-producto-contenedor').css({
-       "left": "-100%"
-     });
-  }
- );
+    /* Suscribo a la coleccion de tarjetas al evento hover */
+    $tarjetas.hover(function(){
+         $(this).find('.tarjeta-imagen-contenedor').css({
+            "transform": "scale(2) rotate(20deg)"
+         });
+         $(this).find('.tarjeta-detalle-producto-contenedor').css({
+            "left": "0"
+         });
+        },
+        function(){
+         $(this).find('.tarjeta-imagen-contenedor').css({
+           "transform": "scale(1) rotate(0)"
+         });
+         $(this).find('.tarjeta-detalle-producto-contenedor').css({
+           "left": "-100%"
+         });
+        }
+    );
 
-  $covervid.height(altoVentana);
+    $covervid.height(altoVentana);
 
-  //seteando el alto de la cabecera de acuerdo al alto de la ventana
-  $cabecera.height(altoVentana);
+    //seteando el alto de la cabecera de acuerdo al alto de la ventana
+    $cabecera.height(altoVentana);
 
-  //inicializando plugin covervid (video como background)
-  $('.covervid-video').coverVid(1920, 1080);
+    //inicializando plugin covervid (video como background)
+    $('.covervid-video').coverVid(1920, 1080);
 });
