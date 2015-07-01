4fa912a7dbb961fd3f906712c5b87a733b6985a8
Pablo Valor <pablovalor89@gmail.com>
2015-07-01 16:14:11 -0300

    Agregado panel mapa

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).on('ready', function(){
     "use strict";
 
-    //cacheando objetos DOM
+    // Cacheando objetos DOM
     var $cabecera   = $('.cabecera');
     var $covervid   = $('.covervid-video');
     var altoVentana = $(window).height();
@@ -17,8 +17,10 @@ $(document).on('ready', function(){
         }
     });
 
-    //Inicializando Carrusel
+    // Inicializando Carrusel
     $('.bxslider').bxSlider({
+        auto: true,
+        autoHover: true,
         mode: 'horizontal',
         infiniteLoop: true,
         tickerHover: true,
@@ -29,34 +31,27 @@ $(document).on('ready', function(){
      $irArriba.on('click', function(){
          $("html, body").animate({scrollTop: 0}, 1000);
      });
+
     /* Suscribo a la coleccion de tarjetas al evento hover */
     $tarjetas.hover(function(){
-        //Escalo al 200% y roto 20 grados en sentido contrario la imagen
-         $(this).find('.tarjeta-imagen-contenedor').css({
-            "transform": "scale(2.5) rotate(-20deg)"
-         });
-         //la capa de detalle se hace visible
-         $(this).find('.tarjeta-detalle-producto-contenedor').css({
-            "left": "0"
-         });
+        // Escalo al 200% y roto 20 grados en sentido contrario la imagen
+         $(this).find('.tarjeta-imagen-contenedor').css({ "transform": "scale(2.5) rotate(-20deg)"});
+         // La capa de detalle se hace visible
+         $(this).find('.tarjeta-detalle-producto-contenedor').css({"left": "0"});
         },
         function(){
-            // La imagen vuelve a su a su estado inicial en el evento mouseleave
-         $(this).find('.tarjeta-imagen-contenedor').css({
-           "transform": "scale(1) rotate(0)"
-         });
-          // LA capa de detalle se oculta en el mouseleave
-         $(this).find('.tarjeta-detalle-producto-contenedor').css({
-           "left": "-100%"
-         });
+        // La imagen vuelve a su a su estado inicial en el evento mouseleave
+         $(this).find('.tarjeta-imagen-contenedor').css({"transform": "scale(1) rotate(0)"});
+          // La capa de detalle se oculta en el mouseleave
+         $(this).find('.tarjeta-detalle-producto-contenedor').css({"left": "-100%"});
         }
     );
 
     $covervid.height(altoVentana);
 
-    //seteando el alto de la cabecera de acuerdo al alto de la ventana
+    // Seteando el alto de la cabecera de acuerdo al alto de la ventana
     $cabecera.height(altoVentana);
 
-    //inicializando plugin covervid (video como background)
+    // Inicializando plugin covervid (video como background)
     $('.covervid-video').coverVid(1920, 1080);
 });
